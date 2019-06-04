ab24dc7a5ca558f6bb5c0efb81618ebaace55fde
anastasia-zaitsewa <anastasia.zaitsewa@gmail.com>
2019-06-04 16:36:27 +0200

    WIP

--- src/android/java/com/pspdfkit/cordova/action/AddAnnotationsAction.java
+++ src/android/java/com/pspdfkit/cordova/action/AddAnnotationsAction.java
@@ -1,7 +1,11 @@
 package com.pspdfkit.cordova.action;
 
+import com.pspdfkit.annotations.AnnotationProvider;
 import com.pspdfkit.cordova.CordovaPdfActivity;
 import com.pspdfkit.cordova.PSPDFKitCordovaPlugin;
+import com.pspdfkit.document.PdfDocument;
+import com.pspdfkit.document.formatters.DocumentJsonFormatter;
+import com.pspdfkit.document.providers.DataProvider;
 
 import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
@@ -14,14 +18,24 @@ import androidx.annotation.NonNull;
 
 public class AddAnnotationsAction extends BasicAction {
 
+  private static final int ARG_ANNOTATIONS_JSON = 0;
+
   public AddAnnotationsAction(@NonNull String name, @NonNull PSPDFKitCordovaPlugin plugin) {
     super(name, plugin);
   }
 
   @Override protected void execAction(JSONArray args, CallbackContext callbackContext) throws JSONException, IOException {
-    final boolean wasModified = CordovaPdfActivity.getCurrentActivity().saveDocument();
-    final JSONObject response = new JSONObject();
-    response.put("wasModified", wasModified);
-    callbackContext.success(response);
+    String annotationJson = args.getJSONObject(ARG_ANNOTATIONS_JSON).toString();
+    PdfDocument document = CordovaPdfActivity.getCurrentActivity().getDocument();
+    if (document != null) {
+//      AnnotationProvider annotationProvider = document.getAnnotationProvider();
+//      annotationProvider.addAnnotationToPage(
+//          annotationProvider.createAnnotationFromInstantJson(annotationJson)
+//      );
+
+//      final DataProvider dataProvider = new (json);
+//      DocumentJsonFormatter.importDocumentJson(fragment.getDocument(), dataProvider);
+      callbackContext.success();
+    }
   }
 }
